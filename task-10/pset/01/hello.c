bf2f58cc1b29796947c1c962c0d8921eda8fa786
adithyarjndrn <89097182+adithyarjndrn@users.noreply.github.com>
2021-11-19 19:35:04 +0530

    Create hello.c

---
+++ task-10/pset/01/hello.c
@@ -0,0 +1,10 @@
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    string name = get_string("What is your name: "); //declare a string with 'name' variable
+
+    printf("hello, %s\n", name); //then print the name with %s 
+}
